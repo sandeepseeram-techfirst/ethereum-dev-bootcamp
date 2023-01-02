0d784e5b62090cf492555ac5ed5fff22d1972d3a
sandeepseeram-techfirst <sandeepseeram@outlook.com>
2023-01-02 13:37:48 +0530

    blockchain structure notes & code

---
+++ Week1/BlockchainStructure/block_hash_calculation.js
@@ -0,0 +1,29 @@
+// const Block = require("./Block.js");
+const crypto = require("crypto");
+
+// class Blockchain {
+  // constructor() { ... }
+  // get() { ... }
+  // get latestBlock() { ... }
+  // isValidHashDifficulty(hash) { ... }
+
+  calculateHashForBlock(block) {
+    const { index, previousHash, timestamp, transactions, nonce } = block;
+    return this.calculateHash(
+      index,
+      previousHash,
+      timestamp,
+      transactions,
+      nonce
+    );
+  }
+
+  calculateHash(index, previousHash, timestamp, data, nonce) {
+    return crypto
+      .createHash("sha256") // SHA256 Hash Function
+      .update(index + previousHash + timestamp + data + nonce)
+      .digest("hex");
+  }
+// };
+
+// module.exports = Blockchain;
---
+++ Week1/BlockchainStructure/notes.md
@@ -0,0 +1,4 @@
+Blockchain Structure 
+Slides: 
+https://docs.google.com/presentation/d/1-FgRVeAHxCMfcH5YAMj8Qpk2gmN3IacVVvncIbVfauo/edit 
+
